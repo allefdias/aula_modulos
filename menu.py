f8a2d02616d1433a97a17c719687587ca570dc61
allefdias <147508740+allefdias@users.noreply.github.com>
2023-11-21 11:42:21 -0300

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,32 @@
+from use_cases.adicionar import adicionarProduto
+from use_cases.editar import editarProduto
+from use_cases.deletar import deletarProduto
+from use_cases.listar import listarProdutos
+
+def menu():
+    while True:
+        print('----MENU DE PRODUTOS----')
+        print('1 - Cadastrar produto')
+        print('2 - Editar produto')
+        print('3 - Deletar produto')
+        print('4 - Listar produtos')
+        print('5 - Sair')
+        opcao = input('Selecione a sua opção: ')
+        if opcao == '1':
+            nome = input('Digite o nome do produto: ')
+            categoria = input('Digite a categoria do produto: ')
+            preco = float(input('Digite o preço do produto: '))
+        elif opcao == '2':
+            codigo = int(input('Digite o código do produto: '))
+            nome = input('Digite o nome do produto: ')
+            categoria = input('Digite a categoria do produto: ')
+            preco = float(input('Digite o preço do produto: '))
+            editarProduto(codigo, nome, categoria, preco)
+        elif opcao == '3':
+            codigo = int(input('Digite o código do produto: '))
+            deletarProduto(codigo)
+        elif opcao == '4':
+            listarProdutos
+        else:
+            print('Você saiu do sistema!')
+menu()
